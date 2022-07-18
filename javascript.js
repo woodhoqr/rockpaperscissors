271585f693a8994cc246040a15f603265d5d573e
woodhoqr <woodhoqr@dukes.jmu.edu>
2022-07-18 15:15:46 -0400

    complete getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function getComputerChoice()
+{
+    let choices = ["Rock", "Paper", "Scissors"];
+    let random = Math.floor(Math.random() * choices.length);
+
+    return choices[random];
+}
+
+function playRound (userInput, computerInput)
+{
+
+}
