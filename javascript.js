9b96392acbb581575ddb7bf12a3942f377a5acd9
woodhoqr <woodhoqr@dukes.jmu.edu>
2022-07-19 14:05:25 -0400

    updating playRound function, added checkDraw helper function

--- javascript.js
+++ javascript.js
@@ -8,5 +8,32 @@ function getComputerChoice()
 
 function playRound (userInput, computerInput)
 {
+    let verdict = " ";
 
+    if (checkDraw(userInput, computerInput))
+    {
+        verdict = `${userInput} & ${computerInput} = a Draw! Play another round!!`;
+    }
+    /*
+        if userInput == rock
+            if computerInput == rock, draw//Done
+            else if computerInput == scissors, user wins
+            else if computerInput == paper, user loses
+
+        if userInput == paper
+            if computerInput == paper, draw//Done
+            else if computerInput == rock, user wins
+            else if computerInput == scissors, user loses
+
+        if userInput == scissors
+            if computerInput == scissors, draw//Done
+            else if computerInput == paper, user wins
+            else if computerInput == rock, user loses
+
+    */
+}
+
+function checkDraw(userInput, computerInput)
+{
+    return (userInput.toLowercase() == computerInput.toLowercase());
 }
