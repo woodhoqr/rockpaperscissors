14491e1179d3190dcb6c3dd37c05a50bf92a2357
woodhoqr <woodhoqr@dukes.jmu.edu>
2022-08-02 19:43:44 -0400

    completely revamped ui, added new methods (isGameOver, createScoreDiv, updateScore), revamped playRound and playGame functions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let userScore = 0;
+let compScore = 0;
+
 function getComputerChoice()
 {
     const choices = ["rock", "paper", "scissors"];
@@ -8,45 +11,53 @@ function getComputerChoice()
 
 function playRound (userInput, computerInput)
 {
-    let verdict = " ";
+    let dVerdict = `Draw! ${userInput} = ${computerInput}! Play another round!!`;
+    let lVerdict = `Losses happen, ${computerInput} beats ${userInput}! Let's go again!!`;
+    let wVerdict = `Winner, winner, chicken dinner!!! ${userInput} beats ${computerInput}! Let's play another round!!`;
     let uInLower = userInput.toLowerCase();
 
     if (checkDraw(uInLower, computerInput))
     {
-        return verdict = `Draw! ${userInput} = ${computerInput}! Play another round!!`;
+        return updateScore(dVerdict);
     }
 
     if (uInLower == "rock")
         if (computerInput == "scissors")
         {
-            return verdict = `Winner, winner, chicken dinner!!! ${userInput} beats ${computerInput}! Let's play another round!!`;
+            userScore++;
+            return isGameOver(wVerdict);
         }
 
         else if (computerInput == "paper")
         {
-            return verdict = `Losses happen, ${computerInput} beats ${userInput}! Let's go again!!`;
+            compScore++;
+            return isGameOver(lVerdict);
         }
 
     if (uInLower == "paper")
         if (computerInput == "rock")
         {
-            return verdict = `Winner, winner, chicken dinner!!! ${userInput} beats ${computerInput}! Let's play another round!!`;
+            userScore++;
+            return isGameOver(wVerdict);
         }
 
         else if (computerInput == "scissors")
         {
-            return verdict = `Losses happen, ${computerInput} beats ${userInput}! Let's go again!!`;
+            compScore++;
+            return isGameOver(lVerdict);
         }
 
     if (uInLower == "scissors")
         if (computerInput == "paper")
         {
-            return verdict = `Winner, winner, chicken dinner!!! ${userInput} beats ${computerInput}! Let's play another round!!`;
+            userScore++;
+            return isGameOver(wVerdict);
         }
 
         else if (computerInput == "rock")
         {
-            return verdict = `Losses happen, ${computerInput} beats ${userInput}! Let's go again!!`;
+            compScore++;
+            return isGameOver(lVerdict);
         }
 }
 
@@ -55,63 +66,91 @@ function checkDraw(userInput, computerInput)
     return (userInput.toLowerCase() == computerInput.toLowerCase());
 }
 
-function checkWinner(verdict)
+function createScoreDiv()
 {
-    return (verdict.charAt(0) == "W");
+    const scoreDiv = document.createElement('div');
+    scoreDiv.setAttribute('id', 'score');
+    document.body.appendChild(scoreDiv);
+
+    const player = document.createElement('h5');
+    player.setAttribute('id', 'ps');
+    player.textContent = "Player Score: " + userScore;
+    scoreDiv.appendChild(player);
+
+    const comp = document.createElement('h5');
+    comp.setAttribute('id', 'cs');
+    comp.textContent = "Computer Score: " + compScore;
+    scoreDiv.appendChild(comp);
+
+    const verdict = document.createElement('h5');
+    verdict.setAttribute('id', 'verdict');
+    scoreDiv.appendChild(verdict);
 }
 
-function playGame()
+function updateScore(verdict)
 {
-    let userI = "";
+    const ps_t = document.getElementById("ps");
+    const cs_t = document.getElementById("cs");
+    const res = document.getElementById('verdict');
+
+    if (compScore == 5 || userScore == 5)
+    {
+        res.style.color = 'red';
+    }
+    
+    ps_t.innerHTML = "Player Score: " + userScore;
+    cs_t.innerHTML = "Computer Score: " + compScore;
+    res.innerHTML = verdict;
+}
+
+function isGameOver(verdict)
+{
+    if (compScore == 5 || userScore == 5)
+    {
+        buttons.forEach((button) => {
+    
+            button.removeEventListener('click', function () { playGame(button.id); });
+            
+            });
+
+
+        if (compScore == 5)
+        {
+            updateScore(verdict = "You LOST. Better luck next time, cap'n!! The computer wins this one");
+        }
+
+        if (userScore == 5)
+        {
+            updateScore(verdict = "You WON. Hot stuff!! You showed that computer what's WHAT");
+        }
+    }
+    else
+    {
+        updateScore(verdict);
+    }
+
+    return ((compScore == 5) || (userScore) == 5);
+}
+
+function playGame(userChoice)
+{
+    let userI = userChoice;
     let verdict = "";
-    let userScore = 0;
-    let compScore = 0;
 
- 
-    for (let i = 0; i < 5; i++)
+    if (isGameOver())
     {
-        userI = prompt("Choose your fighter: ");
-        verdict = playRound(userI, getComputerChoice());
-        if (verdict.charAt(0) == "D")
-        {
-            console.log(verdict);
-        }
-        else
-        {
-            if (checkWinner(verdict))
-            {
-                userScore++;
-            }
-            else
-            {
-                compScore++;
-            }
-
-           console.log(verdict);
-        }
+        return;
     }
 
-    if (userScore == compScore)
-    {
-        console.log("What a CAT...or is that tik tok toe?");
-    }
-
-    else if (userScore > compScore)
-    {
-        console.log("Hot stuff!! You showed that computer what's WHAT");
-    }
-
-    else 
-    {
-        console.log("Better luck next time, cap'n!! The computer wins this one");
-    }
-
-    console.log("Final Score");
-    console.log("User: " + userScore);
-    console.log("Computer: " + compScore);
-    alert(`Final Score: User [${userScore}] \t Computer [${compScore}]`);
+    verdict = playRound(userI, getComputerChoice());
 }
 
-playGame();
+createScoreDiv();
 
+const buttons = document.querySelectorAll('button');
 
+buttons.forEach((button) => {
+    
+button.addEventListener('click', function () { playGame(button.id); });
+    
+});    
